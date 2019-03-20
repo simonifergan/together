b254b1c0496b1b68d66d92d26bc6c0077b81ee2f
Yanai Avnet <bzakabz@gmail.com>
2019-03-20 14:52:21 +0200

    Itsuv

--- frontend/src/views/TripDetails.vue
+++ frontend/src/views/TripDetails.vue
@@ -1,28 +1,50 @@
 <template>
   <section class="trip-details">
-    {{trip}}
-    <!-- <img src="https://via.placeholder.com/250x170" alt=""> -->
+    <div class="main-header-background"></div>
+    <div class="top-fold">
+      <img src="https://api.adorable.io/avatars/285/abott@adorable.png" class="user-avatar">
+      <h1>Title</h1>
+      <h2>user name
+        <!-- {{trip.user.userName}} NAME OF PROP? -->
+      </h2>
+    </div>
+    <ul>
+      Interested users
+      <li v-for="user in interestedUsers" :key="user._id">
+        <!-- {{user.userName}} NAME OF PROP? -->
+      </li>
+    </ul>
+    <p>{{trip.desc}}</p>
+    <div class="destinations">
+      <ul>
+        <li
+          v-for="destination in trip.destinations"
+          :key="destination.region"
+        >{{destination.region}}, {{destination.country}}</li>
+      </ul>
+    </div>
+    <div class="comments">
+      <h3>comments</h3>
+    </div>
   </section>
 </template>
 
 <script>
-
 export default {
-  name: 'trip-details',
-  components: {
-  },
+  name: "trip-details",
+  components: {},
   computed: {
     trip() {
       return this.$store.getters.tripToDisplay;
     }
   },
   created() {
-    var {tripId} = this.$route.params;
-    if (tripId) this.$store.dispatch({type: 'loadTrip', tripId});
+    var { tripId } = this.$route.params;
+    if (tripId) this.$store.dispatch({ type: "loadTrip", tripId });
     else this.$router.go(-1);
   },
   destroy() {
-    this.$store.commit({type: 'clearTrip'});
+    this.$store.commit({ type: "clearTrip" });
   }
-}
+};
 </script>
