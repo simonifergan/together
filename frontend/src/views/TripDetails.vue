5a69a4366e975ec757e121b5eeed301efbd8c525
Yanai Avnet <bzakabz@gmail.com>
2019-03-20 17:42:38 +0200

    sync

--- frontend/src/views/TripDetails.vue
+++ frontend/src/views/TripDetails.vue
@@ -1,30 +1,28 @@
 <template>
   <section class="trip-details">
-    <div class="top-fold">
-      <img src="https://api.adorable.io/avatars/285/abott@adorable.png" class="user-avatar">
-      <h1>Title</h1>
-      <h2>
-        <!-- {{trip.user.firstname}}&nbsp;{{trip.user.lastname}} -->
-      </h2>
-    </div>
-    <ul>
-      Interested users
-      <li v-for="user in interestedUsers" :key="user._id">
-        <!-- {{user.userName}} NAME OF PROP? -->
-      </li>
-    </ul>
-    <p>{{trip.desc}}</p>
-    <div class="destinations">
-      <ul>
-        <li
-          v-for="destination in trip.destinations"
-          :key="destination.region"
-        >{{destination.region}}, {{destination.country}}</li>
+      <div class="top-fold">
+        <img src="https://api.adorable.io/avatars/285/abott@adorable.png" class="user-avatar">
+        <h1>Title</h1>
+        <h2>{{trip.user.firstname}}&nbsp;{{trip.user.lastname}}</h2>
+        <button><i class="far fa-comment-alt"></i></button>
+      </div>
+      <ul>Interested users
+        <li v-for="user in interestedUsers" :key="user._id">
+          <!-- {{user.userName}} NAME OF PROP? -->
+        </li>
       </ul>
-    </div>
-    <div class="comments">
-      <h3>comments</h3>
-    </div>
+      <p>{{trip.desc}}</p>
+      <div class="destinations">
+        <ul>
+          <li
+            v-for="destination in trip.destinations"
+            :key="destination.region"
+          >{{destination.region}}, {{destination.country}}</li>
+        </ul>
+      </div>
+      <div class="comments">
+        <h3>comments</h3>
+      </div>
   </section>
 </template>
 
