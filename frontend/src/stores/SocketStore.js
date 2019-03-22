f02d15f524540f60282dbb7089cb5f3e757a76bc
Simon <simonifergan239@gmail.com>
2019-03-22 16:53:23 +0200

    broken chat

--- frontend/src/stores/SocketStore.js
+++ frontend/src/stores/SocketStore.js
@@ -17,6 +17,10 @@ export default {
         addNewChat(state, { chat }) {
             state.userChats.push(chat);
         },
+        activateChat(state, {chatId}) {
+            const chat = state.userChats.find(chat => chat._id === chatId);
+            if (chat) chat.isActive = true;
+        },
         removeChat(state, { chatId }) {
             const idx = state.userChats.find(chat => chat._id === chatId);
             if (idx === -1) return;
@@ -51,7 +55,10 @@ export default {
         socketConnect(context) {
             context.dispatch({ type: 'socketUserConnect' })
             SocketService.on(SocketService.CHAT_JOIN_NEW, chat => {
-                context.commit({ type: 'addNewChat', chat })
+                    context.commit({ type: 'addNewChat', chat })
+            })
+            SocketService.on(SocketService.CHAT_JOIN, chatId => {
+                    context.commit({ type: 'activateChat', chatId })
             })
             SocketService.on(SocketService.CHAT_RECEIVE_MSG, ({ chatId, msg }) => {
                 context.commit({ type: 'addMsg', msg, chatId });
@@ -80,6 +87,7 @@ export default {
                     loggedUserId: context.getters.loggedUser._id,
                     users: [userId],
                 };
+                context.commit({type: 'activateChat', chatId});
             } else {
                 payload = {
                     chatId: null,
