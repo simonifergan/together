f2f5dfdb26d58222f921bc27f7d915318198bc9b
Adi Binenbaum <adi.binen@gmail.com>
2019-03-23 19:08:56 +0200

    s

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -15,6 +15,8 @@ const API_USER = (process.env.NODE_ENV !== 'development')
     ? '/api'
     : '//localhost:3003/api';
 
+
+    
 async function update(userToUpdate) {    
     const {data} = await axios.put(`${API_USER}/user/${userToUpdate._id}`, userToUpdate)
     return data
