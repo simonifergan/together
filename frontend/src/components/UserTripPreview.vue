1505727bd6c12fd4bb4b8c29baf65dba16d632b6
Simon Ifergan <simonifergan239@gmail.com>
2019-03-25 13:57:42 +0200

    userdetials more

--- frontend/src/components/UserTripPreview.vue
+++ frontend/src/components/UserTripPreview.vue
@@ -1,30 +1,28 @@
 <template>
-  <li class="user-trip-preview">
-    {{trip}}
-    <ul class="member-list" @click="isMembersShowing = !isMembersShowing">
-      <h3>Members</h3>
-      <transition name="fade">
-      <div @click.stop class="accordion-container" v-show="isMembersShowing">
-        <li
-          class="member-item"
-          v-for="(member) in trip.members"
-          :key="member._id+trip._id"
-        >{{member}}</li>
+  <li class="user-trip-preview" @click="isExpanded = !isExpanded" >
+    <h2>{{trip.title}}</h2>
+    <transition name="fade">
+      <div class="expand-container" @click.stop="" v-show="isExpanded">
+        <ul class="pending-list">
+          <h3>Pending</h3>
+          <div @click.stop class="accordion-container">
+            <li class="user-item" v-for="(pender) in trip.pending" :key="pender._id+trip._id">
+              <div class="user-img" :style="{backgroundImage: `url('${pender.profilePic}')`}"/>
+              <span>{{pender.firstname}}&nbsp;{{pender.lastname}}</span>
+            </li>
+          </div>
+        </ul>
+        <ul class="member-list">
+          <h3>Members</h3>
+          <div @click.stop class="accordion-container">
+            <li class="user-item" v-for="(member) in trip.members" :key="member._id+trip._id">
+              <div class="user-img" :style="{backgroundImage: `url('${member.profilePic}')`}"/>
+              <span>{{member.firstname}}&nbsp;{{member.lastname}}</span>
+            </li>
+          </div>
+        </ul>
       </div>
-      </transition>
-    </ul>
-    <ul class="pending-list" @click="isPendingShowing = !isPendingShowing">
-      <h3>Pending</h3>
-      <transition name="fade">
-        <div @click.stop class="accordion-container" v-show="isPendingShowing">
-          <li
-            class="pending-item"
-            v-for="(pender) in trip.pending"
-            :key="pender._id+trip._id"
-          >{{pender}}</li>
-        </div>
-      </transition>
-    </ul>
+    </transition>
   </li>
 </template>
 
@@ -38,6 +36,7 @@ export default {
   },
   data() {
     return {
+      isExpanded: false,
       isPendingShowing: false,
       isMembersShowing: false
     };
