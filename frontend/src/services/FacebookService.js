4834f39dd67d32bf634c040828942f721a25658e
Simon Ifergan <simonifergan239@gmail.com>
2019-03-25 19:09:55 +0200

    fb login

---
+++ frontend/src/services/FacebookService.js
@@ -0,0 +1,36 @@
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '594742284326154',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{api-version}'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+
+// How to start and check if someone is already logged in to facebook
+// RESPONSE OBJECT:
+// {
+//     status: 'connected',
+//     authResponse: {
+//         accessToken: '...',
+//         expiresIn:'...',
+//         signedRequest:'...',
+//         userID:'...'
+//     }
+// }
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
