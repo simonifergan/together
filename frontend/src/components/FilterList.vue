a1e8af52bee89d83f4d9f19a1cbed1323afe47b4
Yanai Avnet <bzakabz@gmail.com>
2019-03-30 15:57:42 +0300

    drag

--- frontend/src/components/FilterList.vue
+++ frontend/src/components/FilterList.vue
@@ -41,7 +41,10 @@ export default {
       page: 0,
       touchPos: null,
       didDrag: false,
-      startPage: null
+      startPage: null,
+      transitionPrm: null,
+      stopScrolling: false,
+      timeout: null
     };
   },
   computed: {
@@ -49,7 +52,7 @@ export default {
       return `${this.page * this.itemWidth * -1}px`;
     },
     pagination() {
-      return { left: this.slidePos };
+      return { transform: `translateX(${this.slidePos})` };
     },
     itemsPerPage() {
       if (window.matchMedia("(min-width: 1100px)").matches) return 5;
@@ -61,43 +64,68 @@ export default {
       return 220;
     },
     isHiddenRight() {
-      if (this.page === this.maxPage) return {opacity: '0'}
+      if (this.page >= this.maxPage) return {opacity: '0'}
       return {opacity: '1'}
     },
     isHiddenLeft() {
-      if (!this.page) return {opacity: '0'}
+      if (this.page <= 0) return {opacity: '0'}
       else return {opacity: '1'}
     },
-    maxPage() {      
-      return this.filters.length - this.itemsPerPage
+    maxPage() {
+      let diff = this.filters.length - this.itemsPerPage
+      return (diff >= 0) ? diff : 0
     }
   },
   methods: {
-    moveSlide(diff) {
-      if (diff === "+" && this.page < this.maxPage) {
-        this.page = this.page + 1;
+    async moveSlide(diff) {
+      await this.transitionPrm
+      console.log('sliding');
+      if (diff === "+" && this.page <= this.maxPage - 1) {
+        this.animateScroll(this.page, this.page + 1)
       } else if (diff === "-" && this.page > 0) {
-        this.page = this.page - 1;
+        this.animateScroll(this.page, this.page - 1)
       }
       return;
     },
     startDrag(ev) {
+      clearTimeout(this.timeout)
+      this.stopScrolling = true
       this.touchPos = ev.touches[0].clientX
       this.startPage = this.page
     },
-    drag(ev) {
+    async drag(ev) {
       ev.preventDefault()
-      
       const diff = (ev.touches[0].clientX - this.touchPos)
       if (diff > 10) this.didDrag = true
       this.page = this.startPage - diff/this.itemWidth
-      console.log(this.page);
     },
     stopDrag() {
-      if (this.page > this.maxPage) this.page = this.maxPage
-      else if (this.page < 0) this.page = 0
-      else this.page = Math.round(this.page)
+      let setPage
+      if (this.page > this.maxPage) setPage = this.maxPage
+      else if (this.page < 0) setPage = 0
+      else setPage = Math.round(this.page)
+      this.animateScroll(this.page, setPage)
       this.didDrag = false
+      this.touchPos = null
+      this.startPage = null
+    },
+    async animateScroll(startPos, endPos) {
+      this.stopScrolling = false
+      this.transitionPrm = new Promise(async (res, rej) => {
+        let i = 0
+        for (let i = 0; i < 1; i+= 0.015) {
+          this.page = endPos - (endPos - startPos)*(1 - (Math.sqrt(1 - (i-1)**2)))
+          await new Promise((timeout, rej) => {
+            this.timeout = setTimeout(timeout, 6)
+          })
+          if (this.stopScrolling) {
+            res()
+            break;
+          }
+        }
+        this.page = endPos
+        res()
+      })
     },
     beforeRouteLeave (to, from, next) {
       if (!didDrag) {
